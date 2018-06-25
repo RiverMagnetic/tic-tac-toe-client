117a277f103d33efa251c42f622c461449a84786
RiverMagnetic <magneticcontinuum@gmail.com>
2018-06-25 18:51:17 -0400

    Add Comments and Beginnings of Functions
    added beginnings of functions for checking if a box is occupied,
    checking the board for a win, a function for marking a box, and an event
    handler for clicking a box. there are a lot of test version stuff going
    on here.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,14 +7,62 @@
 // require('./example')
 
 $(() => {
-// board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-// const love = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'],
-// let turn = '<which player>'
-isMarked: function (index) {
-  if (this.board[index] === 'X' || this.board[index] === 'O') {
-    return true
-  } else {
-    return false
-  }
-},
 })
+
+// set up game board like so:
+//   0 | 1 | 2
+//   3 | 4 | 5
+//   6 | 7 | 8
+const game = {
+  board: ['', '', '', '', '', '', '', '', ''],
+  love: ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'],
+
+  // is the box the user clicked on occupied? (this function is called in events.js)
+  marked: function (index) {
+    if (this.board[index] === 'X' || this.board[index] === 'O') {
+      return true
+    } else {
+      return false
+    }
+  },
+  // check the board to see if there's a winner
+  checkForWin: function () {
+    const box = this.board
+    // win possibilities: rows[0, 1, 2], [3, 4, 5], [6, 7, 8], columns [0,3,6], [1,4,7], [2,5,8], diagonals [0,4,8], [2,4,6]
+    if ((box[0] === box[1] && box[1] === box[2]) || (box[3] === box[4] && box[4] === box[5]) || (box[6] === box[7] && box[7] === box[8]) || (box[0] === box[3] && box[3] === box[6]) || (box[1] === box[4] && box[4] === box[7]) || (box[2] === box[5] && box[5] === box[8]) || (box[0] === box[4] && box[4] === box[8]) || (box[2] === box[4] && box[4] === box[6])) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+// let turn = '<which player>'
+
+const index = game.love[0]
+
+const mark = function (index) {
+  console.log(index)
+
+  // now that you have the index of the cell that was clicked,
+  // and you know which player is currently playing,
+  // find a way to record the move at it's given index for this player
+  // either "x" or "o" ... somewhere
+
+  // if the box the user clicked on is not occupied then proceed
+  if (game.marked(index) === false) {
+    // put X or O in the box
+    console.log('x')
+  }
+}
+const addHandlers = () => {
+  $('.box').on('click', function (event) {
+    event.preventDefault()
+    mark(event.target.id)
+  })
+}
+
+addHandlers()
+
+// module.exports = {
+//   addHandlers
+// }
