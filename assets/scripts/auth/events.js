0200ee8e0eaeea75364c1d9f08e9863223132b08
RiverMagnetic <magneticcontinuum@gmail.com>
2018-06-27 15:40:35 -0400

    Add refactored code/comments from modal lesson
    added code and comments trying to refactor this morning's modal lesson
    for a create account button in case i want to use it. FIX: broken?

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,20 +1,21 @@
 const getFormFields = require('../../lib/get-form-fields')
-// Using your knowledge of jQuery write a function, onSubmitForm, that console
-// logs the input in the input field when "save changes" is clicked
+// Using your knowledge of jQuery write a function, onSubmitForm or openModal, that console logs the input in the input field when "save changes" is clicked
+// this will be referenced in index.js
 
-const openModal = function (event) {
-  event.preventDefault()
-  const data = getFormFields(event.target)
-  console.log(data)
-  console.log(data.form)
-  $('#signUpModal').modal('hide')
-}
-
-// const cancelButton = function (event) {
-//
+// const openModal = function (event) {
+//   event.preventDefault()
+//   const data = getFormFields(event.target)
+//   console.log(data)
+//   console.log(data.form)
+//   $('#signUpModal').modal('hide')
 // }
 
+// testing export to index.js using a console log in a cancel button that I hope to use in the future...
+const cancelButton = function (event) {
+  console.log('cancel creation!')
+}
+
 module.exports = {
-  openModal
-  // cancelButton
+  // openModal
+  cancelButton
 }
