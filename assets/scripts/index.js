9c0883216610e8182bdf9b2878aa562f64a32fb7
RiverMagnetic <magneticcontinuum@gmail.com>
2018-06-28 01:15:54 -0400

    Move code order and un-comment code
    Moved code order in an attempt to get the game to log a win when the
    winning cell is clicked, instead of when a subsequent cell is clicked
    (but this may be an issue to solve with ajax, and or through the api).
    
    un-commented code to get the files talking to each other.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,87 +8,90 @@
 
 const config = require('./config')
 const authEvents = require('./auth/events')
-// const gameEvents = require('./game/events')
+const gameEvents = require('./game/events')
 
 $(() => {
+  // set up game cells like so:
+  //   0 | 1 | 2
+  //   3 | 4 | 5
+  //   6 | 7 | 8
+
+  const game = {
+    cells: ['', '', '', '', '', '', '', '', ''],
+    love: ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'],
+    move: function (index) {
+      this.cells[index] = this.love.pop()
+      return this.cells[index]
+    },
+    // is the cell the player clicked on occupied? (this function is called in game/events.js)
+    marked: function (index) {
+      if (this.cells[index] === 'X' || this.cells[index] === 'O') {
+        return true
+      } else {
+        return false
+      }
+    },
+    // check the cells to see if there's a winner
+    checkForWin: function () {
+      console.log('checking for win')
+      const cell = this.cells
+      console.log(cell)
+      let win = false
+      // win possibilities: rows[0, 1, 2], [3, 4, 5], [6, 7, 8], columns [0,3,6], [1,4,7], [2,5,8], diagonals [0,4,8], [2,4,6]
+      if ((cell[0] !== '' && cell[0] === cell[1] && cell[1] === cell[2]) || (cell[3] !== '' && cell[3] === cell[4] && cell[4] === cell[5]) || (cell[6] !== '' && cell[6] === cell[7] && cell[7] === cell[8]) || (cell[0] !== '' && cell[0] === cell[3] && cell[3] === cell[6]) || (cell[1] !== '' && cell[1] === cell[4] && cell[4] === cell[7]) || (cell[2] !== '' && cell[2] === cell[5] && cell[5] === cell[8]) || (cell[0] !== '' && cell[0] === cell[4] && cell[4] === cell[8]) || (cell[2] !== '' && cell[2] === cell[4] && cell[4] === cell[6])) {
+        win = true
+        console.log(win)
+        return win
+      } else {
+        console.log(win)
+        return win
+      }
+    }
+  }
+
+  // now that you have the index of the cell that was clicked,
+  // and you know which player is currently playing,
+  // find a way to record the move at it's given index for this player
+  // either "x" or "o" ... somewhere
+
+  const mark = function (index) {
+    // console.log(index)
+  // only allow player to mark the board if the game has not been won
+    if (game.checkForWin() === false) {
+    // only allow player to mark the board if the cell player clicked on has not been marked
+    // together, these two if statements disallow player from marking board after game is over
+      if (game.marked(index) === false) {
+        // console.log(game.marked(index))
+        const affection = game.move(index)
+        console.log(affection)
+        // mark the board by placing the letter in the cell
+        $('#' + index).append(affection)
+        // prompting the player to take their turn
+        if (affection === 'X') {
+          $('#message').html(`O's turn!`)
+        } else if (affection === 'O') {
+          $('#message').html(`X's turn!`)
+        }
+        // TODO final mark in game should say "Game Over", not tell next player to take a turn.
+      }
+    }
+  }
+
+  // create a function to hold event handlers
+  const addHandlers = () => {
+    $('.cell').on('click', function (event) {
+      event.preventDefault()
+      mark(event.target.id)
+    })
+  }
+  // call the addHandlers function that was just defined
+  addHandlers()
+
   // authEvents.addHandlers()
-  // // gameEvents.addHandlers()
-  // $('#create-account').on('submit', authEvents.openModal)
+  gameEvents.addHandlers()
+  $('#create-account').on('submit', authEvents.openModal)
   $('#cancel-create-account').on('click', authEvents.cancelSignUpButton)
 })
-
-// set up game cells like so:
-//   0 | 1 | 2
-//   3 | 4 | 5
-//   6 | 7 | 8
-
-const game = {
-  cells: ['', '', '', '', '', '', '', '', ''],
-  love: ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'],
-
-  // is the cell the player clicked on occupied? (this function is called in game/events.js)
-  marked: function (index) {
-    if (this.cells[index] === 'X' || this.cells[index] === 'O') {
-      return true
-    } else {
-      return false
-    }
-  },
-  // check the cells to see if there's a winner
-  checkForWin: function () {
-    console.log('checking for win')
-    const cell = this.cells
-    // win possibilities: rows[0, 1, 2], [3, 4, 5], [6, 7, 8], columns [0,3,6], [1,4,7], [2,5,8], diagonals [0,4,8], [2,4,6]
-    if ((cell[0] !== '' && cell[0] === cell[1] && cell[1] === cell[2]) || (cell[3] !== '' && cell[3] === cell[4] && cell[4] === cell[5]) || (cell[6] !== '' && cell[6] === cell[7] && cell[7] === cell[8]) || (cell[0] !== '' && cell[0] === cell[3] && cell[3] === cell[6]) || (cell[1] !== '' && cell[1] === cell[4] && cell[4] === cell[7]) || (cell[2] !== '' && cell[2] === cell[5] && cell[5] === cell[8]) || (cell[0] !== '' && cell[0] === cell[4] && cell[4] === cell[8]) || (cell[2] !== '' && cell[2] === cell[4] && cell[4] === cell[6])) {
-      console.log(true)
-      return true
-    } else {
-      console.log(false)
-      return false
-    }
-  },
-  move: function (index) {
-    this.cells[index] = this.love.pop()
-    return this.cells[index]
-  }
-}
-
-// now that you have the index of the cell that was clicked,
-// and you know which player is currently playing,
-// find a way to record the move at it's given index for this player
-// either "x" or "o" ... somewhere
-const mark = function (index) {
-  // console.log(index)
-// only allow player to mark the board if the game has not been won
-  if (game.checkForWin() === false) {
-  // only allow player to mark the board if the cell player clicked on has not been marked
-  // together, these two if statements disallow player from marking board after game is over
-    if (game.marked(index) === false) {
-      // console.log(game.marked(index))
-      const affection = game.move(index)
-      console.log(affection)
-      // mark the board by placing the letter in the cell
-      $('#' + index).append(affection)
-      // prompting the player to take their turn
-      if (affection === 'X') {
-        $('#prompt').html(`O's turn!`)
-      } else if (affection === 'O') {
-        $('#prompt').html(`X's turn!`)
-      }
-      // TODO final mark in game should say "Game Over", not prompt next player to take a turn.
-    }
-  }
-}
-
-const addHandlers = () => {
-  $('.cell').on('click', function (event) {
-    event.preventDefault()
-    mark(event.target.id)
-  })
-}
-
-addHandlers()
-
 // module.exports = {
 //   addHandlers
 // }
