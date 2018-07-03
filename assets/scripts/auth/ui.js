74766c9aadd428050eccdb5eaafe4cae79372e16
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-03 18:51:38 -0400

    Uncommented lesson code

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -14,18 +14,18 @@ const signUpFailure = function (error) {
   console.error('signUpFailure ran. Error is :', error)
 }
 
-// const signInSuccess = function (data) {
-//   $('#message').text('Signed in successfully')
-//   $('#message').css('background-color', 'green')
-//   console.log('signInSuccess ran. Data is :', data)
-//   store.user = data.user
-// }
+const signInSuccess = function (data) {
+  $('#message').text('Signed in successfully')
+  $('#message').css('background-color', 'green')
+  console.log('signInSuccess ran. Data is :', data)
+  store.user = data.user
+}
 
-// const signInFailure = function (error) {
-//   $('#message').text('Error on sign in')
-//   $('#message').css('background-color', 'red')
-//   console.error('signInFailure ran. Error is :', error)
-// }
+const signInFailure = function (error) {
+  $('#message').text('Error on sign in')
+  $('#message').css('background-color', 'red')
+  console.error('signInFailure ran. Error is :', error)
+}
 
 // const signOutSuccess = function () {
 //   $('#message').text('Signed out successfully')
@@ -54,10 +54,10 @@ const signUpFailure = function (error) {
 
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
   // ,
-//   signInSuccess,
-//   signInFailure,
 //   signOutSuccess,
 //   signOutFailure,
 //   changePasswordSuccess,
