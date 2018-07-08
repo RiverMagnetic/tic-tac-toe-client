26686b178b83ce8fa6277ae0c57e14765363a7da
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-08 12:06:50 -0400

    Add working create game curl script

---
+++ curl-scripts/game/create.sh
@@ -0,0 +1,9 @@
+# TOKEN=tokengoeshere TEXT=textgoeshere sh curl-scripts/game/create.sh
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games" \
+  --include --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{ "game": { "text": "'"${TEXT}"'" }}'
+
+echo
