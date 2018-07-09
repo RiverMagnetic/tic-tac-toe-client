def1e83e4a3d6d9a807152a3b68c5f8dacbd5c46
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-09 16:19:19 -0400

    Change url, add comments
    Corrected API URL. Added comments

--- curl-scripts/game/update.sh
+++ curl-scripts/game/update.sh
@@ -1,8 +1,20 @@
 
-# ID=2 TOKEN=tokengoeshere TEXT=textgoeshere sh curl-scripts/game/update.sh
+# ID= TOKEN= INDEX=1 VALUE="x" OVER=false sh curl-scripts/game/update.sh
 
-curl "https://wdi-library-api.herokuapp.com/games/${ID}" \
+curl "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
   --include --request PATCH \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{ "game": { "cell": { "index": "'"${INDEX}"'", "value": "'"${VALUE}"'" },"over": "'"${OVER}"'" } }'
+  
+  # Written differently here to visualize what is needed to make this work in the rest of my code:
+  # --data '{ 
+  #     "game": { 
+  #       "cell": { 
+  #         "index": "'"${INDEX}"'", 
+  #         "value": "'"${VALUE}"'" 
+  #       },
+  #       "over": "'"${OVER}"'" 
+  #     }
+  #   }'
+
