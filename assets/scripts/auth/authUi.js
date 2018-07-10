465c2a8ec6f20ed673259dca45dc493262813d5c
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-09 23:23:13 -0400

    Upgrade UI msgs, comment out console msgs

--- assets/scripts/auth/authUi.js
+++ assets/scripts/auth/authUi.js
@@ -5,20 +5,20 @@ const store = require('../store')
 const signUpSuccess = function (data) {
   $('#message').text('Signed up successfully')
   $('#message').css('background-color', 'green')
-  console.log('signUpSuccess ran. Data is :', data)
+  // console.log('signUpSuccess ran. Data is :', data)
   $('#sign-up-modal').hide()
 }
 
 const signUpFailure = function (error) {
-  $('#message').text('Missing email or password')
+  $('#message').text('Incorrect email or password')
   $('#message').css('background-color', 'red')
-  console.error('signUpFailure ran. Error is :', error)
+  // console.error('signUpFailure ran. Error is :', error)
 }
 
 const signInSuccess = function (data) {
-  $('#message').text("x's turn!")
+  $('#message').text("Let's play!")
   $('#message').css('background-color', 'green')
-  console.log('signInSuccess ran. Data is :', data)
+  // console.log('signInSuccess ran. Data is :', data)
   store.user = data.user
   $('#logInModal').modal('hide')
   $('#sign-in-modal').hide()
@@ -32,13 +32,13 @@ const signInFailure = function (error) {
   $('#logInModal').modal('hide')
   $('#message').text('Incorrect Email or Password')
   $('#message').css('background-color', 'red')
-  console.error('signInFailure ran. Error is :', error)
+  // console.error('signInFailure ran. Error is :', error)
 }
 
 const signOutSuccess = function () {
   $('#message').text('Signed out')
   $('#message').css('background-color', 'green')
-  console.log('signOutSuccess ran and nothing was returned!')
+  // console.log('signOutSuccess ran and nothing was returned!')
   store.user = null
   $('#sign-in-modal').show()
   $('#sign-up-modal').show()
@@ -52,20 +52,20 @@ const signOutSuccess = function () {
 const signOutFailure = function (error) {
   $('#message').text('Error on sign out')
   $('#message').css('background-color', 'red')
-  console.error('signOutFailure ran. Error is :', error)
+  // console.error('signOutFailure ran. Error is :', error)
 }
 
 const changePasswordSuccess = function () {
   $('#message').text('Changed password successfully')
   $('#message').css('background-color', 'green')
-  console.log('changePasswordSuccess ran and nothing was returned!')
+  // console.log('changePasswordSuccess ran and nothing was returned!')
   $('#changePasswordModal').hide()
 }
 
 const changePasswordFailure = function (error) {
-  $('#message').text('Error on change password')
+  $('#message').text('Incorrect old or new password')
   $('#message').css('background-color', 'red')
-  console.error('changePasswordFailure ran. Error is :', error)
+  // console.error('changePasswordFailure ran. Error is :', error)
   
 }
 
