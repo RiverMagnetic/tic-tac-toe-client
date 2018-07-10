dd0542c08cda6d9ec65aec385f82fa34a227854a
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-09 23:13:38 -0400

    Comment out console logs

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -28,17 +28,17 @@ const gameEvents = require('./game/gameEvents.js')
     },
     // check the cells to see if there's a winner
     checkForWin: function () {
-      console.log('checking for win')
+      // console.log('checking for win')
       const cell = this.cells
-      console.log(cell)
+      // console.log(cell)
       let win = false
       // win possibilities: rows[0, 1, 2], [3, 4, 5], [6, 7, 8], columns [0,3,6], [1,4,7], [2,5,8], diagonals [0,4,8], [2,4,6]
       if ((cell[0] !== '' && cell[0] === cell[1] && cell[1] === cell[2]) || (cell[3] !== '' && cell[3] === cell[4] && cell[4] === cell[5]) || (cell[6] !== '' && cell[6] === cell[7] && cell[7] === cell[8]) || (cell[0] !== '' && cell[0] === cell[3] && cell[3] === cell[6]) || (cell[1] !== '' && cell[1] === cell[4] && cell[4] === cell[7]) || (cell[2] !== '' && cell[2] === cell[5] && cell[5] === cell[8]) || (cell[0] !== '' && cell[0] === cell[4] && cell[4] === cell[8]) || (cell[2] !== '' && cell[2] === cell[4] && cell[4] === cell[6])) {
         win = true
-        console.log(win)
+        // console.log(win)
         return win
       } else {
-        console.log(win)
+        // console.log(win)
         return win
       }
     },
@@ -50,15 +50,15 @@ const gameEvents = require('./game/gameEvents.js')
   }
   
   const mark = function (index) {
-    console.log(index)
+    // console.log(index)
     // only allow player to mark the board if the game has not been won
     if (game.checkForWin() === false) {
       // only allow player to mark the board if the cell player clicked on has not been marked.
       // together, these two if statements disallow player from marking board after game is over
       if (game.marked(index) === false) {
-        // console.log(game.marked(index))
+        // // console.log(game.marked(index))
         const affection = game.move(index)
-        console.log(affection)
+        // console.log(affection)
         // mark the board by placing the letter in the cell
         $('#' + index).append(affection)
         // prompting the player to take their turn
@@ -79,7 +79,7 @@ const gameEvents = require('./game/gameEvents.js')
         }
         const data = { affection, gameOver, index }
         gameEvents.onUpdateGame(data)
-        console.log(data)
+        // console.log(data)
       // if player tries to put mark in marked cell, prompt to choose another cell
       } else if (game.marked(index) === true) {
         $('#message').html(`That square has already been taken. Choose another.`)
@@ -101,9 +101,9 @@ const gameEvents = require('./game/gameEvents.js')
   // to wipe the board/start a new game
   const createGame = function (event) {
     game.cells = ['', '', '', '', '', '', '', '', '']
-    console.log(game.cells)
+    // console.log(game.cells)
     game.love = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
-    console.log(game.love)
+    // console.log(game.love)
     $('.cell').text('')
     gameEvents.onCreateGame(event)
   }
